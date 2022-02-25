c12133bba9d09a71b31208ace9310d507cb72bd5
doms99 <dominik.mandic@fer.hr>
2022-02-25 18:49:43 +0100

    fetch handling fixes

--- src/redux/thunks.ts
+++ src/redux/thunks.ts
@@ -3,14 +3,21 @@ import { ThunkAction } from "redux-thunk";
 import { setError, setGenres, setTracks } from "./slice"
 import { RootState } from "./store";
 
-const base = "https://cors-anywhere.herokuapp.com/https://api.deezer.com";
-
 export function fetchGenres() {
   const fetchGenresThunk: ThunkAction<void, RootState, void, Action> = (dispatch) => {
 
-    fetch(`${base}/genre`)
-    .then(res => res.json())
+    fetch("/genre")
     .then(res => {
+      if(res.status !== 200) {
+        throw new Error(`Error on /genre`);
+      }
+      return res.json();
+    })
+    .then(res => {
+      if(!res || !res.data) {
+        throw new Error(res);
+      }
+
       dispatch(setGenres(res.data));
     })
     .catch(err => {
@@ -25,9 +32,18 @@ export function fetchGenres() {
 
 export function fetchTracks(id: number) {
   const fetchTracksThunk: ThunkAction<void, RootState, void, Action> = (dispatch) => {
-    fetch(`${base}/chart/${id}/tracks`)
-    .then(res => res.json())
+    fetch(`/chart/${id}/tracks`)
     .then(res => {
+      if(res.status !== 200) {
+        throw new Error(`Error on /chart/${id}/tracks`);
+      }
+      return res.json();
+    })
+    .then(res => {
+      if(!res || !res.data) {
+        throw new Error(res);
+      }
+
       dispatch(setTracks({genreId: id, tracks: res.data}));
     })
     .catch(err => {
